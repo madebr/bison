ec3972d9fab1c4a8d5324291805352b6eb519ffe
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-07-30 18:11:36 +0200

    Use max_int instead of max because MSVC defines this symbol already

--- src/counterexample.c
+++ src/counterexample.c
@@ -113,7 +113,7 @@ free_counterexample (counterexample *cex)
   free (cex);
 }
 
-static int max (int a, int b)
+static int max_int (int a, int b)
 {
   return a < b ? b : a;
 }
@@ -131,8 +131,8 @@ counterexample_print (const counterexample *cex, FILE *out, const char *prefix)
   const char *deriv2_label
     = cex->shift_reduce ? _("Reduce derivation") : _("Second reduce derivation");
   const int width =
-    max (max (mbswidth (example1_label, 0), mbswidth (example2_label, 0)),
-         max (mbswidth (deriv1_label, 0),   mbswidth (deriv2_label, 0)));
+    max_int (max_int (mbswidth (example1_label, 0), mbswidth (example2_label, 0)),
+             max_int (mbswidth (deriv1_label, 0),   mbswidth (deriv2_label, 0)));
   if (flat)
     fprintf (out, "  %s%s%*s ", prefix,
              example1_label, width - mbswidth (example1_label, 0), "");
